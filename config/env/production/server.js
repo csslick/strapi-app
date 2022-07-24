fd4df68df254dc053ca2d336f9cc93887aa54433
csslick <digitone@naver.com>
2022-07-24 23:39:39 +0900

    add key

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env("https://strapi-app-csslick.herokuapp.com/"),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array('APP_KEYS')
   },
 });
