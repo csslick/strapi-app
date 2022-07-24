773806dfed9ea1b2ec162aa568d8e3d1429ad706
csslick <digitone@naver.com>
2022-07-25 00:19:04 +0900

    111

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-  url: env("https://git.heroku.com/strapi-app-csslick.git"),
-  proxy: true,
-  app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
-  },
-});
